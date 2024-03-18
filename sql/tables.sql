3d4025b82c75e53f772e2014cbb318aee9851745
spireneko <spireneko@gmail.com>
2024-03-18 17:25:14 +0400

    Fix tables architecture

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,38 +1,40 @@
-CREATE TYPE user_role AS ENUM ('respondent', 'admin');
-CREATE TYPE question_type AS ENUM ('with_text', 'with_option');
+-- CREATE TYPE user_role AS ENUM ('respondent', 'admin');
+-- CREATE TYPE question_type AS ENUM ('with_text', 'with_option');
 
-CREATE TABLE IF NOT EXISTS "user" (
-    id                  serial unique PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS site_user (
+    id                  serial PRIMARY KEY,
     login               varchar(100) unique NOT NULL,
     password            varchar(100) NOT NULL,
     role                user_role NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS survey (
-    id                  serial unique PRIMARY KEY NOT NULL,
-    creator_id          serial REFERENCES "user"(id) NOT NULL,
-    topic               text NOT NULL
+    id                  serial PRIMARY KEY,
+    creator_id          serial REFERENCES site_user(id) NOT NULL,
+    topic               text NOT NULL,
+    creation_time       timestamp with time zone NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS question (
-    survey_id           serial REFERENCES survey(id),
+    survey_id           serial REFERENCES survey(id) NOT NULL,
     number              smallserial NOT NULL,
     type                question_type,
-    "text"              text,
+    question_text       text,
     PRIMARY KEY (survey_id, number)
 );
 
 CREATE TABLE IF NOT EXISTS pass (
-    id                  serial unique PRIMARY KEY NOT NULL,
+    id                  serial PRIMARY KEY,
     survey_id           serial REFERENCES survey(id) NOT NULL,
-    respondent_id       serial REFERENCES "user"(id) NOT NULL,
-    "timestamp"         timestamp with time zone
+    respondent_id       serial REFERENCES site_user(id) NOT NULL,
+    creation_time       timestamp with time zone NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS answer (
     pass_id             serial REFERENCES pass(id) NOT NULL,
-    "text"              text,
-    survey_id           serial,
+    answer_text         text,
+    survey_id           serial REFERENCES survey(id),
     question_number     smallserial,
-    FOREIGN KEY         (survey_id, question_number) REFERENCES question (survey_id, number)
+    PRIMARY KEY (survey_id, question_number),
+    FOREIGN KEY (survey_id, question_number) REFERENCES question(survey_id, number)
 );
