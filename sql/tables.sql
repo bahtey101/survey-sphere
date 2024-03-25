1d4c75680df3475357eeba9c6e3eed9dfab2af47
spireneko <spireneko@gmail.com>
2024-03-25 16:07:54 +0400

    Rename tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,42 +1,42 @@
 CREATE TYPE user_role AS ENUM ('respondent', 'admin');
 CREATE TYPE question_type AS ENUM ('with_text', 'with_option');
 
-CREATE TABLE IF NOT EXISTS site_user (
+CREATE TABLE IF NOT EXISTS site_users (
     id                  serial PRIMARY KEY,
     login               varchar(100) unique NOT NULL,
     password            varchar(100) NOT NULL,
     role                user_role NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS survey (
+CREATE TABLE IF NOT EXISTS surveys (
     id                  serial PRIMARY KEY,
-    creator_id          serial REFERENCES site_user(id) NOT NULL,
+    creator_id          serial REFERENCES site_users(id) NOT NULL,
     topic               text NOT NULL,
     creation_time       timestamp with time zone NOT NULL,
     UNIQUE (topic, creator_id)
 );
 
-CREATE TABLE IF NOT EXISTS question (
-    survey_id           serial REFERENCES survey(id) NOT NULL,
+CREATE TABLE IF NOT EXISTS questions (
+    survey_id           serial REFERENCES surveys(id) NOT NULL,
     number              smallserial NOT NULL UNIQUE,
     type                question_type,
     question_text       text,
     PRIMARY KEY (survey_id, number)
 );
 
-CREATE TABLE IF NOT EXISTS pass (
+CREATE TABLE IF NOT EXISTS passes (
     id                  serial PRIMARY KEY,
-    survey_id           serial REFERENCES survey(id) NOT NULL,
-    respondent_id       serial REFERENCES site_user(id) NOT NULL,
+    survey_id           serial REFERENCES surveys(id) NOT NULL,
+    respondent_id       serial REFERENCES site_users(id) NOT NULL,
     creation_time       timestamp with time zone NOT NULL,
     UNIQUE (survey_id, respondent_id)
 );
 
-CREATE TABLE IF NOT EXISTS answer (
-    pass_id             serial REFERENCES pass(id) NOT NULL,
+CREATE TABLE IF NOT EXISTS answers (
+    pass_id             serial REFERENCES passes(id) NOT NULL,
     answer_text         text,
-    survey_id           serial REFERENCES survey(id),
+    survey_id           serial REFERENCES surveys(id),
     question_number     smallserial,
     PRIMARY KEY (pass_id, survey_id, question_number),
-    FOREIGN KEY (survey_id, question_number) REFERENCES question(survey_id, number)
+    FOREIGN KEY (survey_id, question_number) REFERENCES questions(survey_id, number)
 );
