87d270a926f5ec757288b96a3e81e6ac5ca4d718
spireneko <spireneko@gmail.com>
2024-05-06 14:50:04 +0400

    Add header

---
+++ front/www/components/Header.jsx
@@ -0,0 +1,20 @@
+import styles from "./styles/header.module.css";
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <nav className={styles.header_container}>
+                <Link className={styles.site_name} href={"/"}>
+                    Survey Sphere
+                </Link>
+
+                <Link className={styles.exit_button} href={"/"}>
+                    Выйти
+                </Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
