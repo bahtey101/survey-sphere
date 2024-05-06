25a189cead3052e648dfdc8b1fdb59932b5e0ba5
spireneko <spireneko@gmail.com>
2024-05-06 19:59:11 +0400

    Fix oversized images

--- front/www/app/mysurveys/SurveyWithImage.jsx
+++ front/www/app/mysurveys/SurveyWithImage.jsx
@@ -1,23 +1,15 @@
 import styles from "@/styles/my_surveys/survey-with-image.module.css";
-import Image from "next/image";
 import BlueButton from "@/components/BlueButton";
 
-const SurveyWithImage = () => {
+const SurveyWithImage = ({ survey_name, image_src }) => {
+    image_src = image_src || "/";
     return (
         <div className={styles.container}>
             <div className={styles.image}>
-                <img
-                    src="/public/Triangles-4k.png"
-                    alt="Картинка не загрузилась :("
-                />
+                <img src={image_src} alt="Картинка не загрузилась :(" />
             </div>
             <div className={styles.place_for_name_and_button}>
-                <div className={styles.survey_name}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Facilis unde ab, in, quia, enim minima nihil alias
-                    exercitationem animi temporibus quae cupiditate commodi
-                    saepe rerum. Sint ipsam quo facilis fugiat!
-                </div>
+                <div className={styles.survey_name}>{survey_name}</div>
                 <div className={styles.button}>
                     <BlueButton text="Просмотреть" />
                 </div>
