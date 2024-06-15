1e825e68d63df1bc40ecc54565b9e9159050985d
spireneko <spireneko@gmail.com>
2024-06-15 20:59:51 +0400

    Add basic PSOT fetching for sign up

--- front/www/components/Input.jsx
+++ front/www/components/Input.jsx
@@ -3,11 +3,12 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
-const Input = ({ type, placeholder, id }) => {
+const Input = ({ type, placeholder, id, name }) => {
     return (
         <input
             type={type}
             placeholder={placeholder}
+            name={name}
             className={styles.input}
             id={id}
         ></input>
