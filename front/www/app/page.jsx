b52575b032e0c097a9ca604e4984341743f8d07b
spireneko <spireneko@gmail.com>
2024-06-16 12:57:43 +0400

    Fix fetching error in home page

--- front/www/app/page.jsx
+++ front/www/app/page.jsx
@@ -1,21 +1,6 @@
 import Link from "next/link";
 
-async function getData() {
-    const res = await fetch("http://localhost:8080/users");
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-
-    if (!res.ok) {
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error("Failed to fetch data");
-    }
-
-    return res.json();
-}
-
 export default async function Home(children) {
-    const data = await getData();
-
     return (
         <main>
             <p>
@@ -33,7 +18,6 @@ export default async function Home(children) {
             <p>
                 <Link href={"/adminpanel"}>Панель администратора</Link>
             </p>
-            <p>User ID is: {data.data.ID}</p>
         </main>
     );
 }
